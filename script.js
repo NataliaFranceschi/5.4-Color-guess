068a0577e026a83c15289030da1d3f8532918bf6
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-05-31 21:26:47 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const circulos = document.getElementsByTagName('div');
+const corPricipal = document.getElementById('rgb-color');
+const resultado = document.getElementById('answer');
 
 function corAleatoria() {
   const r = parseInt(Math.random() * 255, 10);
@@ -9,4 +11,20 @@ function corAleatoria() {
 
 for (let circulo of circulos) {
   circulo.style.backgroundColor = corAleatoria();
+  circulo.addEventListener('click', resultadoEscolha)
+}
+
+function resultadoEscolha(event) {
+  const corEscolhida = getComputedStyle(event.target);
+  if (corEscolhida.backgroundColor.split('rgb')[1] === corPricipal.innerText) {
+    resultado.innerHTML = 'Acertou!';
+  } else {
+    resultado.innerHTML = 'Errou! Tente novamente!';
+  }
+}
+
+window.onload = corCentral;
+function corCentral() {
+  const circuloAleatorio = circulos[Math.floor(Math.random() * 6)];
+  corPricipal.innerText = getComputedStyle(circuloAleatorio).backgroundColor.split('rgb')[1]
 }
