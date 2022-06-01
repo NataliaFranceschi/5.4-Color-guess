ed04694314c270ea02abb6bcae2d97a6bbc47ca7
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-06-01 11:41:50 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const circulos = document.getElementsByTagName('div');
 const corPricipal = document.getElementById('rgb-color');
 const resultado = document.getElementById('answer');
 const resetar = document.getElementById('reset-game');
+const placar = document.getElementById('score');
 
 function corAleatoria() {
   const r = parseInt(Math.random() * 255, 10);
@@ -10,18 +11,25 @@ function corAleatoria() {
   return `rgb(${r},${g},${b})`;
 }
 
-for (let circulo of circulos) {
-  circulo.style.backgroundColor = corAleatoria();
-  circulo.addEventListener('click', resultadoEscolha)
+function corCirculos() {
+  for (let circulo of circulos) {
+    circulo.style.backgroundColor = corAleatoria();
+    circulo.addEventListener('click', resultadoEscolha)
+  }
 }
+corCirculos();
+
+let soma = 0;
+placar.innerHTML = `Pontos: ${soma}`;
 
 function resultadoEscolha(event) {
   const corEscolhida = getComputedStyle(event.target);
   if (corEscolhida.backgroundColor.split('rgb')[1] === corPricipal.innerText) {
     resultado.innerHTML = 'Acertou!';
+    soma += 3;
   } else {
     resultado.innerHTML = 'Errou! Tente novamente!';
-  }
+  } placar.innerHTML = `Pontos: ${soma}`;
 }
 
 window.onload = corCentral;
@@ -31,7 +39,9 @@ function corCentral() {
 }
 
 function reiniciarJogo() {
-  location.reload();
+  corCirculos();
+  corCentral();
+  resultado.innerText = 'Escolha uma cor';
 }
 
 resetar.addEventListener('click', reiniciarJogo);
